48470d5a6585a671b18968accb3232e17e97d807
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-12 10:18:30 -0500

    associations complete

--- server/db/index.js
+++ server/db/index.js
@@ -3,14 +3,17 @@
 // with your models, for which you'll find some blank files in this directory:
 
 const db = require('./database');
-const Student = require('./student');
-const Campus = require('./campus');
+const Student = require('./Student');
+const Campus = require('./Campus');
 
 // This is a great place to establish associations between your models
 // (https://sequelize-guides.netlify.com/association-types/).
 // Example:
 //
 // Puppy.belongsTo(Owner)
+
+Student.belongsTo(Campus);
+Campus.hasMany(Student);
 
 module.exports = {
   // Include your models in this exports object as well!
