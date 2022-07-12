105bcc0ca2ed38b847fa54e7686a55d1d454ace0
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-12 10:40:55 -0500

    wrote campuses sub-reducer to manage campuses

--- app/redux/campuses.js
+++ app/redux/campuses.js
@@ -1,9 +1,29 @@
-export const setCampuses = () => {};
+import axios from 'axios';
+import { SET_CAMPUS } from './singleCampus';
 
-export const fetchCampuses = () => {};
+const SET_CAMPUSES = 'SET CAMPUSES';
+
+export const setCampuses = (payload) => ({
+  type: SET_CAMPUSES,
+  payload,
+});
+
+export const fetchCampuses = () => async (dispatch) => {
+  const campusesResponse = await axios.get('/api/candies');
+  dispatch(setCampuses(campusesResponse.data));
+};
 
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
-export default function campusesReducer() {
-  return null;
+export default function campusesReducer(state = [], action) {
+  switch (action.type) {
+    case SET_CAMPUSES:
+      return action.payload;
+    case SET_CAMPUS:
+      return state.map((campus) => {
+        return campus.id === action.payload.id ? action.payload : campus;
+      });
+    default:
+      return state;
+  }
 }
