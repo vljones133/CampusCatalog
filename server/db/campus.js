a09a9ad1e0c1be27505b94054822f7fb6951d5a2
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-12 09:56:40 -0500

    wrote campuses model

---
+++ server/db/campus.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('./database');
+
+module.exports = db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: '.../public/grace-hopper-logo.jpeg',
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+});
--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,10 @@
+'use strict';
 // The purpose of this module is to bring your Sequelize instance (`db`) together
 // with your models, for which you'll find some blank files in this directory:
 
-const db = require('./database')
-const Student = require('./student')
-const Campus = require('./campus')
+const db = require('./database');
+const Student = require('./student');
+const Campus = require('./campus');
 
 // This is a great place to establish associations between your models
 // (https://sequelize-guides.netlify.com/association-types/).
@@ -16,4 +17,4 @@ module.exports = {
   db,
   Student,
   Campus,
-}
+};
