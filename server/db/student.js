86b626dede01d9dc827c2c981e6950ca07fc7532
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-12 10:11:20 -0500

    wrote students model

---
+++ server/db/student.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+const db = require('./database');
+
+module.exports = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: '.../public/grace-hopper-logo.jpeg',
+  },
+  gpa: {
+    type: Sequelize.DECIMAL,
+    validate: {
+      max: 4.0,
+      min: 0.0,
+    },
+  },
+});
