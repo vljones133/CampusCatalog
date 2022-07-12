6686744ae436df775a3f3b97a270e1a5db074f35
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-12 11:31:36 -0500

    completed Routes file that has nav links and routes to components

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import AllCampuses from './AllCampuses';
+import AllStudents from './AllStudents';
 
 const Routes = () => {
   return (
     <Router>
       <div>
-        <nav>Welcome!</nav>
+        <div>
+          <nav>
+            <Link to="/">Welcome!</Link>
+            <Link to="/campuses">Campuses</Link>
+            <Link to="/students">Students</Link>
+          </nav>
+        </div>
         <main>
-          <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
-          <p>This seems like a nice place to get started with some Routes!</p>
+          <Switch>
+            <Route exact path="/">
+              <div>
+                <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
+                <p>
+                  This seems like a nice place to get started with some Routes!
+                </p>
+              </div>
+            </Route>
+            <Route exact path="/campuses" component={AllCampuses} />
+            <Route exact path="/students" component={AllStudents} />
+            {/* <Route path="/candies/:id" component={Candy} /> */}
+          </Switch>
         </main>
       </div>
     </Router>
