f2235e934eb4f124d7fd994641717db0a4bbef0a
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-13 14:47:35 -0500

    added update campus component to single campus view

--- app/components/Campus.js
+++ app/components/Campus.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchCampus } from '../redux/singleCampus';
+import { updateCampusThunk, fetchCampus } from '../redux/singleCampus';
+import UpdateCampus from './UpdateCampus';
+import store from '../store';
 
 class Campus extends React.Component {
   componentDidMount() {
@@ -13,28 +15,33 @@ class Campus extends React.Component {
     const { campus } = this.props;
     const students = campus.students;
     return (
-      <div key={campus.id}>
-        <img src={campus.imageUrl} alt="image of campus" />
-        <h2>{campus.name}</h2>
-        <p>{campus.address}</p>
-        <p>{campus.description}</p>
-        <h3>Student's Enrolled:</h3>
-        <ul>
-          {students && students.length > 0 ? (
-            students.map((student) => {
-              return (
-                <li key={student.id}>
-                  <Link to={`/students/${student.id}`}>
-                    {student.firstName} {student.lastName}
-                  </Link>
-                </li>
-              );
-            })
-          ) : (
-            <p>Sorry, no students to see yet!</p>
-          )}
-        </ul>
-      </div>
+      <main className="singlePage">
+        <aside>
+          <UpdateCampus store={store} />
+        </aside>
+        <div key={campus.id}>
+          <img src={campus.imageUrl} alt="image of campus" />
+          <h2>{campus.name}</h2>
+          <p>{campus.address}</p>
+          <p>{campus.description}</p>
+          <h3>Student's Enrolled:</h3>
+          <ul>
+            {students && students.length > 0 ? (
+              students.map((student) => {
+                return (
+                  <li key={student.id}>
+                    <Link to={`/students/${student.id}`}>
+                      {student.firstName} {student.lastName}
+                    </Link>
+                  </li>
+                );
+              })
+            ) : (
+              <p>Sorry, no students to see yet!</p>
+            )}
+          </ul>
+        </div>
+      </main>
     );
   }
 }
@@ -45,7 +52,7 @@ const mapStateToProps = ({ campus }) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   getCampus: (id) => dispatch(fetchCampus(id)),
-  // updateCampus: (id, campus) => dispatch(updateCampus(id, campus)),
+  updateCampus: (id, campus) => dispatch(updateCampusThunk(id, campus)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Campus);
